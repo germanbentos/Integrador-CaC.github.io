fa4781671c11cd1ab67bba8255c35f8116b2ef72
GermanBentos <geb_axie2020@outlook.com>
2024-05-27 12:42:05 -0300

    Primer commit

---
+++ Principal/principal-js.js
@@ -0,0 +1,27 @@
+function ejecutar(parametro) {
+    const path = "https://image.tmdb.org/t/p/original/";
+  
+    const contenedor = document.getElementById("contenedor");
+  
+    let URL_API =
+      "https://api.themoviedb.org/3/search/movie?api_key=6ef51d843bfe08d642d218c1c25c2915&language=es-ES&query=" +
+      parametro;
+  
+    fetch(URL_API)
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => {
+        console.log(resultado);
+        contenedor.innerHTML = "";
+        for (const pelicula of resultado.results) {
+          if (pelicula.poster_path) {
+            contenedor.innerHTML += `
+                  <div id='tarjeta' >
+                  <div id='contenedorImagen'><img  src=${path}${pelicula.poster_path} id='imagen' ></div>
+                  <div id='texto'><p>${pelicula.title}</p></div>
+                  </div>
+                  `;
+          }
+        }
+      });
+  }
+  ejecutar("lo ultimo");
