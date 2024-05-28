2af57635dc9783a59d312e607595171eed0ed55d
germanbentos <130008314+germanbentos@users.noreply.github.com>
2024-05-28 20:41:07 -0300

    arreglo js

--- Principal/principal-js.js
+++ Principal/principal-js.js
@@ -4,24 +4,38 @@ function ejecutar(parametro) {
     const contenedor = document.getElementById("contenedor");
   
     let URL_API =
-      "https://api.themoviedb.org/3/search/movie?api_key=6ef51d843bfe08d642d218c1c25c2915&language=es-ES&query=" +
-      parametro;
+      "https://api.themoviedb.org/3/search/movie?api_key=6ef51d843bfe08d642d218c1c25c2915&language=es-ES&query=${encodeURIComponent(parametro)}";
   
     fetch(URL_API)
-      .then((respuesta) => respuesta.json())
-      .then((resultado) => {
-        console.log(resultado);
-        contenedor.innerHTML = "";
-        for (const pelicula of resultado.results) {
-          if (pelicula.poster_path) {
-            contenedor.innerHTML += `
-                  <div id='tarjeta' >
-                  <div id='contenedorImagen'><img  src=${path}${pelicula.poster_path} id='imagen' ></div>
-                  <div id='texto'><p>${pelicula.title}</p></div>
-                  </div>
-                  `;
-          }
+     .then((respuesta) => {
+      if (!respuesta.ok) {
+        throw new Error(`HTTP error! status: ${respuesta.status}`);
+      }
+      return respuesta.json();
+    })
+    .then((resultado) => {
+      console.log(resultado);
+      contenedor.innerHTML = "";
+      for (const pelicula of resultado.results) {
+        if (pelicula.poster_path) {
+          contenedor.innerHTML += `
+            <div class='tarjeta'>
+              <div class='contenedorImagen'>
+                <img src="${path}${pelicula.poster_path}" class='imagen' alt='Poster de ${pelicula.title}'>
+              </div>
+              <div class='texto'>
+                <p>${pelicula.title}</p>
+              </div>
+            </div>
+          `;
         }
-      });
-  }
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      contenedor.innerHTML = "<p>Error al cargar los datos. Por favor, intenta de nuevo más tarde.</p>";
+    });
+}
+
+ejecutar("lo ultimo");
   ejecutar("lo ultimo");
